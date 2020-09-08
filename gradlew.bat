4124050ab528044c2012e000f24237a832392be8
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2020-09-08 07:40:31 +0000

    Translate /po/budgie-extras.pot in sr_RS

--- gradlew.bat
+++ gradlew.bat
@@ -5,4 +5,5 @@
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windo
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
