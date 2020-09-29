5f14507cd014c97021796e440d99fc7b4d46920d
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2020-09-29 18:02:28 +0000

    Create null

--- gradlew.bat
+++ gradlew.bat
@@ -53,4 +53,7 @@ if not "%OS%" == "Windows_NT" goto win9xME_args
 :win9xME_args
 @rem Slurp the command line arguments.
 set CMD_LINE_ARGS=
-set _SKIP=
+set _SKIP=2
+
+:win9xME_args_slurp
+if "x%~1" == "x"
