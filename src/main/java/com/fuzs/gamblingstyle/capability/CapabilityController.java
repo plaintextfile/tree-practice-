49c8103c172d8d08011d418bf6268deb9c577384
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2020-10-23 16:32:17 +0000

    Update .rultor.yml

--- src/main/java/com/fuzs/gamblingstyle/capability/CapabilityController.java
+++ src/main/java/com/fuzs/gamblingstyle/capability/CapabilityController.java
@@ -3,4 +3,4 @@ package com.fuzs.gamblingstyle.capability;
 import com.fuzs.gamblingstyle.GamblingStyle;
 import com.fuzs.gamblingstyle.capability.container.ITradingInfo;
 import com.fuzs.gamblingstyle.capability.container.TradingInfo;
-import com.fuzs.ga
+import com.fuzs.gamblingstyle.capability.core.CapabilityDisp
