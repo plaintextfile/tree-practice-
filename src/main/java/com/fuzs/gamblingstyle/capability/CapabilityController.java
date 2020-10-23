ddaec966c81a5787f96964d766f5b898924258c5
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2020-10-23 10:31:24 +0000

    fixed page title

--- src/main/java/com/fuzs/gamblingstyle/capability/CapabilityController.java
+++ src/main/java/com/fuzs/gamblingstyle/capability/CapabilityController.java
@@ -2,4 +2,4 @@ package com.fuzs.gamblingstyle.capability;
 
 import com.fuzs.gamblingstyle.GamblingStyle;
 import com.fuzs.gamblingstyle.capability.container.ITradingInfo;
-import
+import com.fuzs.gamblingstyle.capability.c
