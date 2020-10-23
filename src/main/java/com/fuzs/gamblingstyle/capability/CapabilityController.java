ad8bf86fc4b01d8edd8393de442fa4a4afcd5ed3
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2020-10-23 14:26:53 +0000

    Fix minor memory leak

--- src/main/java/com/fuzs/gamblingstyle/capability/CapabilityController.java
+++ src/main/java/com/fuzs/gamblingstyle/capability/CapabilityController.java
@@ -2,4 +2,5 @@ package com.fuzs.gamblingstyle.capability;
 
 import com.fuzs.gamblingstyle.GamblingStyle;
 import com.fuzs.gamblingstyle.capability.container.ITradingInfo;
-import com.fuzs.gamblingstyle.capability.c
+import com.fuzs.gamblingstyle.capability.container.TradingInfo;
+import com.fuzs.ga
