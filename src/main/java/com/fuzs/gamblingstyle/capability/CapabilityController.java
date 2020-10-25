037e13a02ad03e11ff482ab31f73eb3604fb599b
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2020-10-25 07:41:21 +0000

    Minor doc update

--- src/main/java/com/fuzs/gamblingstyle/capability/CapabilityController.java
+++ src/main/java/com/fuzs/gamblingstyle/capability/CapabilityController.java
@@ -4,4 +4,5 @@ import com.fuzs.gamblingstyle.GamblingStyle;
 import com.fuzs.gamblingstyle.capability.container.ITradingInfo;
 import com.fuzs.gamblingstyle.capability.container.TradingInfo;
 import com.fuzs.gamblingstyle.capability.core.CapabilityDispatcher;
-import com.fuzs.gamblingstyle.capability.c
+import com.fuzs.gamblingstyle.capability.core.CapabilityStorage;
+import net.minecraft.entity
