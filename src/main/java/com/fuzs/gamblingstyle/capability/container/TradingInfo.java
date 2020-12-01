c968b6d7e725e081c42121fe9719c65c7eb3e495
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2020-12-01 06:12:48 +0000

    Updating submodule references in Console to point to latest commits

--- src/main/java/com/fuzs/gamblingstyle/capability/container/TradingInfo.java
+++ src/main/java/com/fuzs/gamblingstyle/capability/container/TradingInfo.java
@@ -1,2 +1,45 @@
 
 package com.fuzs.gamblingstyle.capability.container;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+public class TradingInfo implements ITradingInfo {
+
+    private int lastTradeIndex;
+    private FilterMode filterMode = FilterMode.ALL;
+    private byte[] favoriteTrades = new byte[0];
+
+    @Override
+    public int getLastTradeIndex() {
+
+        return this.lastTradeIndex;
+    }
+
+    @Override
+    public void setLastTradeIndex(int lastTradeIndex) {
+
+        this.lastTradeIndex = lastTradeIndex;
+    }
+
+    @Override
+    public FilterMode getFilterMode() {
+
+        return this.filterMode;
+    }
+
+    @Override
+    public void setFilterMode(FilterMode filterMode) {
+
+        this.filterMode = filterMode;
+    }
+
+    @Override
+    public byte[] getFavoriteTrades() {
+
+        return this.favoriteTrades;
+    }
+
+    @Override
+    public void setFavoriteTrades(byte[] favoriteTrades) {
+
+        this.favoriteTrades = favoriteTrades;
