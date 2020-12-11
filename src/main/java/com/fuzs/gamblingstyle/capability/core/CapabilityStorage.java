a38ee13e668b93e006470c503796ba290c75118e
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2020-12-11 18:22:45 +0000

    Update character

--- src/main/java/com/fuzs/gamblingstyle/capability/core/CapabilityStorage.java
+++ src/main/java/com/fuzs/gamblingstyle/capability/core/CapabilityStorage.java
@@ -3,4 +3,5 @@ package com.fuzs.gamblingstyle.capability.core;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
-import net.minecraftforge.common.capabilities.Capabil
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTS
