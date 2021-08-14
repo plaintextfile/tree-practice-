53abde6f731872b9762c7b5fbbef8e1ed38f1b9b
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2021-08-14 19:48:14 +0000

    Fix typography

--- src/main/java/com/fuzs/gamblingstyle/client/gui/GuiButtonTradingRecipe.java
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/GuiButtonTradingRecipe.java
@@ -84,4 +84,4 @@ public class GuiButtonTradingRecipe extends GuiButton implements ITooltipButton 
 
             RenderHelper.enableGUIStandardItemLighting();
             GlStateManager.disableLighting();
- 
+            mc.getTextureManager().bindTexture(RECIPE
