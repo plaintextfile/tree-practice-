1e152ed7c27757c5f46179c850c091a8459ebc9b
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-02-12 14:53:26 +0000

    Bump version: 0.1.1 → 0.2.0

--- src/main/java/com/fuzs/gamblingstyle/client/gui/GuiTradingBook.java
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/GuiTradingBook.java
@@ -33,3 +33,49 @@ public class GuiTradingBook extends Gui implements IGuiExtension {
     public static final int MAX_BUTTONS = 6;
 
     private Minecraft mc;
+    private final int xSize = 112;
+    private final int ySize = 166;
+    public int hoveredSlot;
+    private ITooltipButton hoveredButton;
+    private final GuiButtonTradingRecipe[] tradeButtons = new GuiButtonTradingRecipe[MAX_BUTTONS];
+    private GuiTextField searchField;
+    private final GuiButtonFilter filterButton;
+    private String lastSearch = "";
+    private int guiLeft;
+    private int guiTop;
+    private boolean requiresRefresh;
+    private TradingRecipeList tradingRecipeList;
+    // Amount scrolled in Creative mode inventory (0 = top, 1 = bottom)
+    private float currentScroll;
+    private int scrollPosition;
+    private boolean isScrolling;
+    private boolean wasClicking;
+    private GuiButton clickedButton;
+    private int selectedTradingRecipe;
+    private boolean clearSearch;
+    private int timesInventoryChanged;
+
+    public GuiTradingBook(ITradingInfo.FilterMode filterMode) {
+
+        this.filterButton = new GuiButtonFilter(this.tradeButtons.length, this.guiLeft + 94, this.guiTop + 8, filterMode);
+        for (int i = 0; i < this.tradeButtons.length; ++i) {
+
+            this.tradeButtons[i] = new GuiButtonTradingRecipe(i, this.guiLeft + 10, this.guiTop + 24 + 22 * i);
+        }
+    }
+
+    @Override
+    public void initGui(Minecraft mc, int width, int height) {
+
+        this.mc = mc;
+        this.requiresRefresh = true;
+        Keyboard.enableRepeatEvents(true);
+        this.guiLeft = (width - this.xSize) / 2 - 88;
+        this.guiTop = (height - this.ySize) / 2;
+        this.timesInventoryChanged = this.mc.player.inventory.getTimesChanged();
+        this.initSearchField();
+
+        this.filterButton.setPosition(this.guiLeft + 94, this.guiTop + 8);
+        for (int i = 0; i < this.tradeButtons.length; i++) {
+
+            this.tradeButtons[i].setPosition(this.guiLeft + 10, this.guiTop + 24 + 22 * i);
