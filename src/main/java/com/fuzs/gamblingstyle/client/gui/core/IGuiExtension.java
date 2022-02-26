b3a175cb619534f0a7cdeddc624e9a366bbdef90
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-02-26 23:09:11 +0000

    Increasing version number

--- src/main/java/com/fuzs/gamblingstyle/client/gui/core/IGuiExtension.java
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/core/IGuiExtension.java
@@ -10,3 +10,19 @@ public interface IGuiExtension {
     void initGui(Minecraft mc, int width, int height);
 
     void onGuiClosed();
+
+    void updateScreen(MerchantRecipeList merchantrecipelist, ContainerVillager container);
+
+    void drawScreen(int mouseX, int mouseY, float partialTicks);
+
+    boolean mouseClicked(int mouseX, int mouseY, int mouseButton);
+
+    void mouseReleased(int mouseX, int mouseY, int state);
+
+    void handleMouseInput();
+
+    boolean hasClickedOutside(int mouseX, int mouseY, int guiLeft, int guiTop, int xSize, int ySize);
+
+    boolean keyTyped(char typedChar, int keyCode);
+
+}
---
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipe.java
@@ -0,0 +1,13 @@
+
+package com.fuzs.gamblingstyle.client.gui.data;
+
+import com.fuzs.gamblingstyle.capability.container.ITradingInfo;
+import com.google.common.collect.Lists;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
