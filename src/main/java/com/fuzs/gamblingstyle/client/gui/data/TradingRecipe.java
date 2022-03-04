9987ada64e73dc04d5c0d6946b32f74e29a7e515
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-03-04 10:43:57 +0000

    fixed linting errors

--- src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipe.java
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipe.java
@@ -11,3 +11,55 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
+
+@SideOnly(Side.CLIENT)
+public class TradingRecipe {
+
+    private final ItemStack[] recipe = new ItemStack[3];
+    private boolean visible = true;
+    private boolean selected;
+    private boolean favorite;
+    int itemIngredients;
+    int secondItemIngredients;
+
+    public TradingRecipe(ItemStack itemToBuy, ItemStack secondItemToBuy, ItemStack itemToSell) {
+
+        this.recipe[0] = itemToBuy;
+        this.recipe[1] = secondItemToBuy;
+        this.recipe[2] = itemToSell;
+    }
+
+    public ItemStack getItemToBuy() {
+
+        return this.recipe[0];
+    }
+
+    public ItemStack getSecondItemToBuy() {
+
+        return this.recipe[1];
+    }
+
+    public boolean hasSecondItemToBuy() {
+
+        return !this.getSecondItemToBuy().isEmpty();
+    }
+
+    public ItemStack getItemToSell() {
+
+        return this.recipe[2];
+    }
+
+    public boolean isVisible() {
+
+        return this.visible;
+    }
+
+    public void setVisible(boolean visible) {
+
+        this.visible = visible;
+    }
+
+    public boolean isSelected() {
+
+        return this.selected;
+    }
