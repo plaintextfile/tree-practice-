a79fae1a6f7fb17948dca67f288118ea96e5f8a7
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-03-04 15:36:10 +0000

    ALSA: usb-audio: Fix memory leak in FTU quirk

--- src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipe.java
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipe.java
@@ -63,3 +63,59 @@ public class TradingRecipe {
 
         return this.selected;
     }
+
+    public void setSelected(boolean selected) {
+
+        this.selected = selected;
+    }
+
+    public boolean isFavorite() {
+
+        return this.favorite;
+    }
+
+    public void favorite() {
+
+        this.favorite = true;
+    }
+
+    public void toggleFavorite() {
+
+        this.favorite = !this.favorite;
+    }
+
+    private boolean isSale() {
+
+        return this.getItemToBuy().getItem() == Items.EMERALD || this.getSecondItemToBuy().getItem() == Items.EMERALD;
+    }
+
+    private boolean isPurchase() {
+
+        return this.getItemToSell().getItem() == Items.EMERALD;
+    }
+
+    public boolean shouldBeIncluded(ITradingInfo.FilterMode mode) {
+
+        switch (mode) {
+
+            case FAVORITES:
+
+                return this.isFavorite();
+            case SELLS:
+
+                return this.isSale();
+            case BUYS:
+
+                return this.isPurchase();
+        }
+
+        return true;
+    }
+
+    public boolean hasRecipeContents() {
+
+        boolean secondItem = !this.hasSecondItemToBuy() || (this.secondItemIngredients >= this.getSecondItemToBuy().getCount());
+        return secondItem && this.itemIngredients >= this.getItemToBuy().getCount();
+    }
+
+    public List<String> getSearchTooltip(EntityPlayer player, ITooltipFlag tooltipFlag) {
