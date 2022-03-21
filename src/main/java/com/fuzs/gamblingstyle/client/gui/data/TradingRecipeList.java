c00f973e57cca4539ba4cd0daeea44b178b4e37d
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-03-21 18:53:33 +0000

    Update azure-pipelines.yml for Azure Pipelines [skip ci]

--- src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipeList.java
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipeList.java
@@ -2,4 +2,5 @@ package com.fuzs.gamblingstyle.client.gui.data;
 
 import com.fuzs.gamblingstyle.capability.container.ITradingInfo;
 import com.fuzs.gamblingstyle.inventory.ContainerVillager;
-import net.mi
+import net.minecraft.client.Minecraft;
+import net.minec
