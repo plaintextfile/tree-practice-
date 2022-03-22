59122b62aa9c26e9738f92634144693d61622916
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-03-22 12:02:02 +0000

    багфикс

--- src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipeList.java
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipeList.java
@@ -3,4 +3,5 @@ package com.fuzs.gamblingstyle.client.gui.data;
 import com.fuzs.gamblingstyle.capability.container.ITradingInfo;
 import com.fuzs.gamblingstyle.inventory.ContainerVillager;
 import net.minecraft.client.Minecraft;
-import net.minec
+import net.minecraft.client.util.ITooltipFlag;
+import net.mine
