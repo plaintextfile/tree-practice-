74d8ff9263e39c65dec6051675051af6ec3d6557
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-04-24 17:38:00 +0000

    - updates for release

--- src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipeList.java
+++ src/main/java/com/fuzs/gamblingstyle/client/gui/data/TradingRecipeList.java
@@ -45,4 +45,6 @@ public class TradingRecipeList extends ArrayList<TradingRecipe> {
         return (int) this.stream().filter(TradingRecipe::isVisible).count();
     }
 
-    public void search(Minecraft mc, String query, ITradingInfo.FilterMode filterMode
+    public void search(Minecraft mc, String query, ITradingInfo.FilterMode filterMode) {
+
+        ITooltipFlag tooltipFlag = mc
