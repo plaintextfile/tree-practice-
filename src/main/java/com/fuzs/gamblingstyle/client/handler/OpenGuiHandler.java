79b81a0260bceeb261ec4a3ff29642e59fce1d2b
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-05-26 08:57:07 +0000

    Tidying up.

--- src/main/java/com/fuzs/gamblingstyle/client/handler/OpenGuiHandler.java
+++ src/main/java/com/fuzs/gamblingstyle/client/handler/OpenGuiHandler.java
@@ -3,4 +3,5 @@ package com.fuzs.gamblingstyle.client.handler;
 import com.fuzs.gamblingstyle.client.gui.GuiVillager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMerchant;
-import net.minec
+import net.minecraftforge.client.event.GuiOpenEvent;
+import 
