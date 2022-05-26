07fa45b1531fb3a078c344046eb57fa82fd68b5a
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-05-26 08:10:44 +0000

    md/raid1: fix use-after-free bug in RAID1 data-check code.

--- src/main/java/com/fuzs/gamblingstyle/client/handler/OpenGuiHandler.java
+++ src/main/java/com/fuzs/gamblingstyle/client/handler/OpenGuiHandler.java
@@ -2,4 +2,5 @@ package com.fuzs.gamblingstyle.client.handler;
 
 import com.fuzs.gamblingstyle.client.gui.GuiVillager;
 import net.minecraft.client.Minecraft;
-import 
+import net.minecraft.client.gui.GuiMerchant;
+import net.minec
