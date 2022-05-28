7b4660c1c53d709ef3b175fdb506b743e0913b26
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-05-28 07:47:42 +0000

    Add another test

--- src/main/java/com/fuzs/gamblingstyle/client/handler/OpenGuiHandler.java
+++ src/main/java/com/fuzs/gamblingstyle/client/handler/OpenGuiHandler.java
@@ -4,4 +4,6 @@ import com.fuzs.gamblingstyle.client.gui.GuiVillager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMerchant;
 import net.minecraftforge.client.event.GuiOpenEvent;
-import net.minecraftforge.fml.common
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+publi
