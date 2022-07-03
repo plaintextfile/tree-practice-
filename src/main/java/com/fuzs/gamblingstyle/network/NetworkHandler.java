acfb1cf35bbf956b84a43c89dfc3d1178e82d7c1
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-07-03 08:47:20 +0000

    update license and readme

--- src/main/java/com/fuzs/gamblingstyle/network/NetworkHandler.java
+++ src/main/java/com/fuzs/gamblingstyle/network/NetworkHandler.java
@@ -8,4 +8,6 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
-import java.util.concurrent.atomic.AtomicIn
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SuppressWarnings("unused")
