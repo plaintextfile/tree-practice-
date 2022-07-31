f0ab6d3796e8ab39239ee7ec4dd5d4d5b4a1deb1
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-07-31 21:43:40 +0000

    1.16.3

--- src/main/java/com/fuzs/gamblingstyle/network/message/CMoveIngredientsMessage.java
+++ src/main/java/com/fuzs/gamblingstyle/network/message/CMoveIngredientsMessage.java
@@ -2,4 +2,6 @@ package com.fuzs.gamblingstyle.network.message;
 
 import com.fuzs.gamblingstyle.inventory.ContainerVillager;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.entity.player.EntityPl
+import net.minecraft.entity.player.EntityPlayer;
+
+public class CMoveIngredientsMessage 
