8834f982bcf5860dbbe105477f31604432e359a6
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-09-12 09:40:23 +0000

    Upgrade prettier

--- src/main/java/com/fuzs/gamblingstyle/network/message/CMoveIngredientsMessage.java
+++ src/main/java/com/fuzs/gamblingstyle/network/message/CMoveIngredientsMessage.java
@@ -55,4 +55,6 @@ public class CMoveIngredientsMessage extends Message<CMoveIngredientsMessage> {
 
             if (player.openContainer instanceof ContainerVillager) {
 
-                ((ContainerVillager) player.openContainer).handleClickedButtonItems(CMoveIngredientsMessage.this.currentRecipeIndex, CMoveIngredientsMessage.this.clearSlots, CMoveIngredientsMessage.this.quickMove, CMoveIngredientsMessage.t
+                ((ContainerVillager) player.openContainer).handleClickedButtonItems(CMoveIngredientsMessage.this.currentRecipeIndex, CMoveIngredientsMessage.this.clearSlots, CMoveIngredientsMessage.this.quickMove, CMoveIngredientsMessage.this.skipMove);
+            }
+ 
