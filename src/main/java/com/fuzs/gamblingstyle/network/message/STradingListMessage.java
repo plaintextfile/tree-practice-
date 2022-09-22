e1522dca6d68f73b0b3f47fff4ab3bc4f52f477d
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-09-22 08:35:58 +0000

    fmt

--- src/main/java/com/fuzs/gamblingstyle/network/message/STradingListMessage.java
+++ src/main/java/com/fuzs/gamblingstyle/network/message/STradingListMessage.java
@@ -2,4 +2,5 @@ package com.fuzs.gamblingstyle.network.message;
 
 import com.fuzs.gamblingstyle.GamblingStyle;
 import com.fuzs.gamblingstyle.client.gui.GuiVillager;
-import io.netty.buf
+import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Mine
