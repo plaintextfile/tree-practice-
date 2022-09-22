c0ebf7382f4633963a4973c2954d2155eab1a14e
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-09-22 12:36:47 +0000

    af_unix: Convert the unix_sk macro to an inline function for type safety

--- src/main/java/com/fuzs/gamblingstyle/network/message/STradingListMessage.java
+++ src/main/java/com/fuzs/gamblingstyle/network/message/STradingListMessage.java
@@ -3,4 +3,5 @@ package com.fuzs.gamblingstyle.network.message;
 import com.fuzs.gamblingstyle.GamblingStyle;
 import com.fuzs.gamblingstyle.client.gui.GuiVillager;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.client.Mine
+import net.minecraft.client.Minecraft;
+import net.minecraft.clie
