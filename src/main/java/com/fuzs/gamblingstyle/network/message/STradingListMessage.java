79fea286ae5afc42c4a05133de566be04e83adff
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-11-12 05:32:06 +0000

    response

--- src/main/java/com/fuzs/gamblingstyle/network/message/STradingListMessage.java
+++ src/main/java/com/fuzs/gamblingstyle/network/message/STradingListMessage.java
@@ -56,4 +56,4 @@ public class STradingListMessage extends Message<STradingListMessage> {
     @Override
     protected MessageProcessor createProcessor() {
 
-  
+        return new TradingListProcessor()
