a03a28995e275f954aa96849414d83f27db7d4df
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2022-12-12 10:59:40 +0000

    * Fixed compiler warnings

--- src/main/java/com/fuzs/gamblingstyle/network/message/STradingListMessage.java
+++ src/main/java/com/fuzs/gamblingstyle/network/message/STradingListMessage.java
@@ -69,4 +69,6 @@ public class STradingListMessage extends Message<STradingListMessage> {
                 Minecraft mc = Minecraft.getMinecraft();
                 int windowId = STradingListMessage.this.data.readInt();
                 GuiScreen screen = mc.currentScreen;
-                if (screen instanceof GuiVillager && windowId == mc.pla
+                if (screen instanceof GuiVillager && windowId == mc.player.openContainer.windowId) {
+
+         
