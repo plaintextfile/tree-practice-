dccbf20ad567aec93f033ea1a119e5e72adb465c
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2023-01-13 12:20:08 +0000

    Version 1.7.4

--- src/main/java/com/fuzs/gamblingstyle/proxy/ClientProxy.java
+++ src/main/java/com/fuzs/gamblingstyle/proxy/ClientProxy.java
@@ -2,4 +2,5 @@ package com.fuzs.gamblingstyle.proxy;
 
 import com.fuzs.gamblingstyle.client.handler.OpenGuiHandler;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import net.min
