ef919f2a7f413ccca4e47b03595e3b6558f3794c
plaintextfile <117562822+plaintextfile@users.noreply.github.com>
2023-03-01 09:44:14 +0000

    Tweaked README

--- src/main/java/com/fuzs/gamblingstyle/proxy/IProxy.java
+++ src/main/java/com/fuzs/gamblingstyle/proxy/IProxy.java
@@ -3,4 +3,6 @@ package com.fuzs.gamblingstyle.proxy;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.IThreadListener;
-import net.minecraftforge.fml.common.event.FMLInitializat
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import javax.annotation.Nonnull;
